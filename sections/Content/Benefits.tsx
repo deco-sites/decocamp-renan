f86854a976692259d38ca86dc798bcf3914b6c65
renan04lima <renan04lima@outlook.com>
2024-06-16 11:36:10 -0300

    🎨 style: lint

--- sections/Content/Benefits.tsx
+++ sections/Content/Benefits.tsx
@@ -54,10 +54,9 @@ export default function Benefits(props: Props) {
   const listOfBenefits = benefits.map((benefit, index) => {
     const showDivider = index < benefits.length - 1;
     const reverse = layout?.variation === "Color reverse";
-    const benefitLayout =
-      !layout?.variation || layout?.variation === "Simple"
-        ? "tiled"
-        : "piledup";
+    const benefitLayout = !layout?.variation || layout?.variation === "Simple"
+      ? "tiled"
+      : "piledup";
 
     return (
       <div
@@ -105,22 +104,24 @@ export default function Benefits(props: Props) {
 
   return (
     <>
-      {!layout?.variation || layout?.variation === "Simple" ? (
-        <div class="w-full container px-4 py-8 flex flex-col gap-8 lg:gap-10 lg:py-10 lg:px-0">
-          <Header
-            title={title}
-            description={description}
-            alignment={layout?.headerAlignment || "center"}
-          />
-          <div class="w-full flex justify-center">
-            <div class="flex flex-col gap-4 lg:gap-8 w-full lg:grid grid-flow-col auto-cols-fr">
-              {listOfBenefits}
+      {!layout?.variation || layout?.variation === "Simple"
+        ? (
+          <div class="w-full container px-4 py-8 flex flex-col gap-8 lg:gap-10 lg:py-10 lg:px-0">
+            <Header
+              title={title}
+              description={description}
+              alignment={layout?.headerAlignment || "center"}
+            />
+            <div class="w-full flex justify-center">
+              <div class="flex flex-col gap-4 lg:gap-8 w-full lg:grid grid-flow-col auto-cols-fr">
+                {listOfBenefits}
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        ""
-      )}
+        )
+        : (
+          ""
+        )}
       {layout?.variation === "With border" && (
         <div class="w-full container flex flex-col px-4 py-8 gap-8 lg:gap-10 lg:py-10 lg:px-0">
           <Header
